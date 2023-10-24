d188ecdb1e92637580945c37268d0ebbbc42bc11
ecurzola <146590441+ecurzola@users.noreply.github.com>
2023-10-24 06:27:18 +0000

    completed

--- app.rb
+++ app.rb
@@ -2,8 +2,56 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/rock") do
+  @our_move = "rock"
+  moves = ["rock","paper","scissors"]
+  @comp_moves = moves.sample
+
+  if @comp_moves == "rock"
+    @outcome = "tied"
+  elsif @comp_moves == "paper"
+    @outcome = "lost"
+  else 
+    @outcome = "won"
+  end
+
+  erb(:zebra)
+  
+end
+
+get("/paper") do
+  @our_move = "paper"
+  moves = ["rock","paper","scissors"]
+  @comp_moves = moves.sample
+
+  if @comp_moves == "rock"
+    @outcome = "won"
+  elsif @comp_moves == "paper"
+    @outcome = "tied"
+  else 
+    @outcome = "lost"
+  end
+
+  erb(:zebra)
+
+end
+
+get("/scissors") do
+  @our_move = "scissors"
+  moves = ["rock","paper","scissors"]
+  @comp_moves = moves.sample
+
+  if @comp_moves == "rock"
+    @outcome = "lost"
+  elsif @comp_moves == "paper"
+    @outcome = "won"
+  else 
+    @outcome = "tied"
+  end
+
+  erb(:zebra)
+
 end
